b951852afbb37088cec183f1c637e8e4212f4f53
ElJosecito <imjose0314@gmail.com>
2023-10-16 00:54:18 -0400

    updated

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,11 @@
-import Router from './Router'
-
-
+import Router from "./Router";
 
 function App() {
-
-
-  //variables
-
-  //funciones
- 
-
-
   return (
     <>
-      
-        <Router />
-      
+      <Router />
     </>
-  )
+  );
 }
 
-export default App
+export default App;
