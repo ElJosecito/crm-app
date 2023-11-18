d4d6e2dae437e74f2cb6cc04e88d871d5b1e1c8b
ElJosecito <imjose0314@gmail.com>
2023-11-18 14:01:26 -0400

    api changed

--- src/config.js
+++ src/config.js
@@ -1,7 +1,8 @@
 export const config = {
     appConfig : {
-        port: 8080,
-        host: 'http://localhost'
+        port: '//crm-political-api.onrender.com',
+        host: 'http'
     }
 }
 
+//https://crm-political-api.onrender.com/api/voters/getAllWithCoordinator
